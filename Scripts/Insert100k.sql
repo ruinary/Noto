e30e06109b2dbe12b085a8986eb3a9d1b2198a1e
ruinary <90007395+ruinary@users.noreply.github.com>
2022-12-20 00:57:49 +0300

    finished??

--- Scripts/Insert100k.sql
+++ Scripts/Insert100k.sql
@@ -1,9 +1,9 @@
-------------------------INSERT 100 000 USERS-------------------------
+------------------------INSERT 100 000 -------------------------
 CREATE OR REPLACE PROCEDURE insert_100k_users
 IS
 BEGIN
     FOR i IN 1 .. 100000 LOOP
-         REGISTER_USER('user' || i, 'pass' || i, 'UserName'|| i, 'UserLastName'|| i, '+375291234567', 'user@mail.ru');
+         REGISTER_USER('user' || i, 'pass' || i, 'UserName'|| i, 'UserLastName'|| i, '+375291234567', 'user@mail.ru'); commit;
     END LOOP;
 END insert_100k_users;
 
@@ -15,8 +15,16 @@ END;
 CREATE OR REPLACE PROCEDURE insert_100k_tasks
 IS
 BEGIN    
-    BEGIN  create_team('qusest','team_demo'); commit; END;
+    BEGIN  create_team('remeral','team_demo'); commit; END;
     FOR i IN 1 .. 100000 LOOP
-         create_task('qusest','team_demo','task_title'||i,1,1,'task_desc'||i, TO_CHAR(SYSDATE, 'DD.MM.YYYY'));
+         create_task('remeral','team_demo','task_title'||i,1,1,'task_desc'||i, TO_CHAR(SYSDATE, 'DD.MM.YYYY')); commit;
     END LOOP;
 END insert_100k_tasks;
+
+CREATE OR REPLACE PROCEDURE insert_100k_teams
+IS
+BEGIN
+    FOR i IN 1 .. 100000 LOOP
+         create_team('remeral', 'team' || i); commit;
+    END LOOP;
+END insert_100k_teams;
